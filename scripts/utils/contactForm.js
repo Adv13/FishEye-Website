7e9f400cfb6c5db22155f848dc984844199c78dd
Adv <tiffanydpro923@gmail.com>
2021-11-21 17:14:22 +0100

    refonte index.html

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,53 +1,9 @@
-export const modalDisplay = () => {
-	// Get the modal
-	const modal = document.getElementById("myModal");
+function displayModal() {
+    const modal = document.getElementById("contact_modal");
+	modal.style.display = "block";
+}
 
-	// Get the button that opens the modal
-	const contactBtn = document.getElementById("myBtn");
-
-	// Get the <span> element that closes the modal
-	const closeBtn = document.getElementsByClassName("modal-contact__close")[0];
-
-	// When the user clicks on the button, open the modal
-	contactBtn.onclick = function () {
-		modal.style.display = "block";
-		modal.classList.add("modal-visible");
-		const firstTabModal = document.querySelector("#headingModal");
-		firstTabModal.focus();
-	};
-
-	// When the user clicks on <span> (x), close the modal
-	closeBtn.onclick = function () {
-		modal.style.display = "none";
-		document.querySelector(".photographInfos__name").focus();
-	};
-
-	// Listen enter too
-	closeBtn.addEventListener("keydown", function (e) {
-		if (e.code == "Enter") {
-			closeBtn.click();
-		}
-	});
-
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function (event) {
-		if (event.target == modal) {
-			modal.style.display = "none";
-		}
-	};
-
-	const form = document.getElementById("form");
-	const inputs = document.querySelectorAll(".modal-contact__inputs");
-
-	// Consolelog inputs
-	function validate(event) {
-		event.preventDefault();
-		modal.style.display = "none";
-		inputs.forEach((input) => {
-			console.log(input.value);
-		});
-		document.querySelector(".photographInfos__name").focus();
-	}
-
-	form.addEventListener("submit", validate);
-};
+function closeModal() {
+    const modal = document.getElementById("contact_modal");
+    modal.style.display = "none";
+}
--- scripts/utils/dataFetched.js
+++
@@ -1,7 +0,0 @@
-export const getData = () =>
-	fetch("./FishEyeDataFR.json")
-		.then((response) => response.json())
-		.catch((error) => {
-			console.error("Une erreur est survenue pendant l'accès aux données.");
-			console.error(error);
-		});

21a5bae106558fd5bf7e38800e5bdad322be97a9
Adv <tiffanydpro923@gmail.com>
2021-12-20 19:49:14 +0100

    suppr modules et maj files organization + maj content index.js

--- scripts/pages/modules/dataFetched.js
+++
@@ -1,9 +0,0 @@
-var requestURL = '../data/photographers.json'
-export const getData = () =>
-fetch(requestURL)
-    .then((response) => response.json())
-    .catch((error) => {
-        console.error("Une erreur est survenue pendant l'accès aux données.");
-        console.error(error);
-    });
-

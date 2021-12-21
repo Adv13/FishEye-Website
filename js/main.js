cfed64f52bbb7c72b6c40758d4a0740507410d52
Adv <tiffanydpro923@gmail.com>
2021-12-21 17:20:21 +0100

    maj main.js file with comments

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict';// code ci-dessous exécuté de manière stricte pour ne pas utiliser de variables non déclarées pour éviter certaines erreurs
 /////////////////////////////////////////
 
 // DATA
@@ -7,27 +7,11 @@ import ApiFishEye from './provider/ApiFishEye.js';
 // HOMEPAGE
 import HomePageBuilder from './home/HomePageBuilder.js';
 
-// PH PAGES
-//import PhotographerProfil from './photographers/PhotographerProfil.js';
-//import DropDownMenu from './photographers/DropDownSort.js';
-//import MediaBuilder from './photographers/MediaBuilder.js';
-
-(function appDispatch() {
-    new ApiFishEye().getDataFishEye().then((data) => {
-        if (window.location.pathname.includes("/photographers.html")) {
-            // PHOTOGRAPHER PROFIL HEADER
-            new PhotographerProfil().displayPhotographerProfil(data);
-
-            // DROPDOWN MENU
-            new DropDownMenu().dropDown(data);
-
-            //PHOTOGRAPHER GALLERY & LIKES BOX
-            new MediaBuilder().photographersMedias(data);
-            return
-        }
+(function appDispatch() { // function pour dispatacher les données sur la page web
+    new ApiFishEye().getDataFishEye().then((data) => { //afficher nouvel object de la class ApiFishEye et récupérer les données
         // HOMEPAGE (PHOTOGRAPHERS, SCROLL, FILTER)
-        new HomePageBuilder().displayPhotographers(data);
+        new HomePageBuilder().displayPhotographers(data);//sinon, afficher un nouvel object home page avec les données des photographes
     }).catch((err) => {
-        console.error('Failed to load ApiFishEye ' + err.message);
+        console.error('Failed to load ApiFishEye ' + err.message); // si problème de chargement des données, afficher msg d'erreur dans console
     })
 })();

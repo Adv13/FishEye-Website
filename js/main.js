2d470920264f00038ea6fa5b1a794b6698e12c7b
Adv <tiffanydpro923@gmail.com>
2021-12-24 18:40:13 +0100

    ajout debut mise en place photographers.html

--- js/main.js
+++ js/main.js
@@ -7,8 +7,24 @@ import ApiFishEye from './provider/ApiFishEye.js';
 // HOMEPAGE
 import HomePageBuilder from './home/HomePageBuilder.js';
 
+// PH PAGES
+//import PhotographerProfil from './photographers/PhotographerProfil.js';
+//import DropDownMenu from './photographers/DropDownSort.js';
+//import MediaBuilder from './photographers/MediaBuilder.js';
+
 (function appDispatch() { // function pour dispatacher les données sur la page web
     new ApiFishEye().getDataFishEye().then((data) => { //afficher nouvel object de la class ApiFishEye et récupérer les données
+        //if (window.location.pathname.includes("/photographers.html")) {//sil'url includes /photographers.html alors
+            // PHOTOGRAPHER PROFIL HEADER
+            //new PhotographerProfil().displayPhotographerProfil(data); //afficher nouvel object with les données du photographe concerné
+
+            // DROPDOWN MENU
+            //new DropDownMenu().dropDown(data);
+
+            //PHOTOGRAPHER GALLERY & LIKES BOX
+            // new MediaBuilder().photographersMedias(data);
+            //return
+        //}
         // HOMEPAGE (PHOTOGRAPHERS, SCROLL, FILTER)
         new HomePageBuilder().displayPhotographers(data);//sinon, afficher un nouvel object home page avec les données des photographes
     }).catch((err) => {

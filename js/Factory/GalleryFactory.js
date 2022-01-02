3a0323edca4efc64d7431ccfd14e0cdf70915d2a
Adv <tiffanydpro923@gmail.com>
2022-01-02 16:24:33 +0100

    maj files gallery et medias

--- js/Factory/GalleryFactory.js
+++ js/Factory/GalleryFactory.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict';// code ci-dessous exécuté de manière stricte pour ne pas utiliser de variables non déclarées pour éviter certaines erreurs
 /////////////////////////////////////////
 /////////////////////////////////////////////
 import MediaFactory from './MediaFactory.js';
@@ -6,21 +6,21 @@ import Lightbox from '../photographers/LightBox.js';
 
 export default class GalleryFactory {// export default permet d'exporter une class, variable et/ou fonction en dehors du fichier avec le même nom
     constructor() {
-        this.totalLike = 0;
+        this.totalLike = 0;//met le compteur de likes à 0
     }
     
     // build the gallery with the different medias and the lightbox
-    builder(dataMedia) {
+    builder(dataMedia) {//récupère les datas concernant les medias pour :
         const id = window.location.search.split('id=')[1];
-        let mediaFactory = new MediaFactory();
-        let currentMedia = [];
-        let currentMediaName = [];
+        let mediaFactory = new MediaFactory();//attribuer une nouvelle instance MediaFactory dans une variable du même nom
+        let currentMedia = []; //créer un tableau sous currentMedia
+        let currentMediaName = [];//créer un tableau sous currentMediaName
 
-        dataMedia.forEach(element => {
-            if (id == element.photographerId) {
-                let sectionPhWorks = document.getElementById('ph-works');
-                let articlePhWork = document.createElement("article");
-                let mediaHTML = mediaFactory.renderMedia(element);
+        dataMedia.forEach(element => {//dans dataMedia, pour chaque element :
+            if (id == element.photographerId) {// si id == l'id du photographe alors:
+                let sectionPhWorks = document.getElementById('ph-works');//mettre l'element avec l'id "ph-works" dans une variable
+                let articlePhWork = document.createElement("article");//créer un element "article" sous la variable "articlePhWork"
+                let mediaHTML = mediaFactory.renderMedia(element);//mettre mediaFactory avec function renderMedia sur element sous une variable
                 let workTemplate = `
                 <a href='#' title=${element.photoName}>
                 ${mediaHTML.outerHTML}
@@ -36,16 +36,16 @@ export default class GalleryFactory {// export default permet d'exporter une cla
                     </div>
                 </div>
                 `
-                articlePhWork.innerHTML = workTemplate;
-                sectionPhWorks.appendChild(articlePhWork);
-                articlePhWork.classList.add("ph-work-elt");
-                this.totalLike += parseInt(element.likes);
-                currentMedia.push(mediaHTML.outerHTML);
-                currentMediaName.push(element.photoName);
-                (new Lightbox())
-                .init(currentMedia, currentMediaName)
+                articlePhWork.innerHTML = workTemplate;//ajouter le morceau de code ci-dessus au code html de la page
+                sectionPhWorks.appendChild(articlePhWork);//ajouter articlePhWork à la sectionPhWork du html de la page
+                articlePhWork.classList.add("ph-work-elt");//ajouter la class "ph-work-elt" à articlePhWork
+                this.totalLike += parseInt(element.likes);//mettre le compteur de likes à jour avec le nombre de likes présents dans le fichier json
+                currentMedia.push(mediaHTML.outerHTML);//pousser de la variable currentMedia les infos medias dans un element et tout ses descendants inclus
+                currentMediaName.push(element.photoName);//pousser de la variable currentMediaName les infos medias dans un element et tout ses descendants inclus
+                (new Lightbox())//nouvelle instance Lightbox dans laquelle
+                .init(currentMedia, currentMediaName)// on initialise les varaibles des medias
             }
         })
-        return this;
+        return this;//retourner resultat
     }
 }
--- js/Factory/MediaFactory.js
+++ js/Factory/MediaFactory.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict';// code ci-dessous exécuté de manière stricte pour ne pas utiliser de variables non déclarées pour éviter certaines erreurs
 /////////////////////////////////////////
 
 import ImageFactory from './ImageFactory.js';
@@ -6,13 +6,13 @@ import VideoFactory from './VideoFactory.js';
 
 export default class MediaFactory {// export default permet d'exporter une class, variable et/ou fonction en dehors du fichier avec le même nom
     // Check if the selected item is an image or a video
-    renderMedia(element) {
-        let factory = null;
-        if (element.hasOwnProperty('image')) {
-            factory = new ImageFactory();
-        } else if (element.hasOwnProperty('video')) {
-            factory = new VideoFactory();
+    renderMedia(element) {//creation de l'element suivant :
+        let factory = null;//attribuer valeur nulle à la variable factory
+        if (element.hasOwnProperty('image')) {//si l'element à la propriété "image" alors
+            factory = new ImageFactory();// factory devient une nouvelle instance de ImageFactory (ce qui créé un element image)
+        } else if (element.hasOwnProperty('video')) {//sinon, si l'element à la propriété "video" alors
+            factory = new VideoFactory();// factory devient une nouvelle instance de VideoFactory (ce qui créé un element video)
         }
-        return factory.createHTML(element);
+        return factory.createHTML(element);//retourner factory en créant l'element
     }
 }

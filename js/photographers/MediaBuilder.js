ecc55e366260af17955717115209bbf1ebeca487
Adv <tiffanydpro923@gmail.com>
2022-01-03 21:07:44 +0100

    maj files mediaBuilder

--- js/photographers/MediaBuilder.js
+++ js/photographers/MediaBuilder.js
@@ -6,26 +6,26 @@ import LikeSubscriber from './Likes.js';
 
 export default class MediaBuilder {// export default permet d'exporter une class, variable et/ou fonction en dehors du fichier avec le même nom
     // call the GalleryFactory to create the media section with 'Like' function and the box
-    photographersMedias(data) {
-        let media = data.media;
-        let gallery = new GalleryFactory().builder(media);
-        this.boxLikesAndPrice(gallery.totalLike, data.photographers);
-        new LikeSubscriber();
+    photographersMedias(data) {//appliquer la function aux data
+        let media = data.media;//mettre les datas des medias dans une variable
+        let gallery = new GalleryFactory().builder(media);//instancier nouvelle gallery avec les données medias dans la variable
+        this.boxLikesAndPrice(gallery.totalLike, data.photographers);//appliquer la function qui gère likes et price
+        new LikeSubscriber();//instancier neo like
     }
 
     // creates a box containing the total number of likes and the photographer's price
-    boxLikesAndPrice(totalLike, photographers) {
-        const id = window.location.search.split('id=')[1];
+    boxLikesAndPrice(totalLike, photographers) {//function avec les paramètres totalLike et photographers
+        const id = window.location.search.split('id=')[1];//mettre les id dans une variable id
 
-        photographers.forEach(element => {
-            if (id == element.id) {
-                let box = document.getElementById('box');
+        photographers.forEach(element => {//pour chaque element dans photographers
+            if (id == element.id) {//si variable id = element id
+                let box = document.getElementById('box');//mettre l'element avec l'id 'box' dans variable
                 let boxTemplate = `
                 <span id="total-likes">${totalLike}</span>
                 <i class="fas fa-heart" aria-label='likes'></i>
                 <span>${element.price} €/ jour</span>
                 `
-                box.innerHTML = boxTemplate;
+                box.innerHTML = boxTemplate;//ajouter dans box le morceau de code html ci-dessus
             }
         })
     }
